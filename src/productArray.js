7a8016e91bfd82cfb92c7f910b12173b80aed610
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-05 15:02:02 +0330

    create file for products

---
+++ src/productArray.js
@@ -0,0 +1,69 @@
+// create Array
+const productsArray = [
+  {
+    id: 1,
+    name: "Cappuccino",
+    image:
+      "https://csyxkpbavpcrhwqhcpyy.supabase.co/storage/v1/object/public/assets/coffee-challenge/cappuccino.jpg",
+    price: 5.2,
+    rating: 4.7,
+    votes: 65,
+    popular: true,
+    available: true,
+  },
+  {
+    id: 2,
+    name: "House Coffee",
+    image:
+      "https://csyxkpbavpcrhwqhcpyy.supabase.co/storage/v1/object/public/assets/coffee-challenge/house-coffee.jpg",
+    price: 3.5,
+    rating: 4.85,
+    votes: 15,
+    popular: true,
+    available: true,
+  },
+  {
+    id: 3,
+    name: "Espresso",
+    image:
+      "https://csyxkpbavpcrhwqhcpyy.supabase.co/storage/v1/object/public/assets/coffee-challenge/espresso.jpg",
+    price: 2.5,
+    rating: 4.9,
+    votes: 55,
+    popular: false,
+    available: true,
+  },
+  {
+    id: 4,
+    name: "Coffee Latte",
+    image:
+      "https://csyxkpbavpcrhwqhcpyy.supabase.co/storage/v1/object/public/assets/coffee-challenge/coffee-latte.jpg",
+    price: 4.5,
+    rating: 5,
+    votes: 23,
+    popular: false,
+    available: true,
+  },
+  {
+    id: 5,
+    name: "Chocolate Coffee",
+    image:
+      "https://csyxkpbavpcrhwqhcpyy.supabase.co/storage/v1/object/public/assets/coffee-challenge/chocolate-coffee.jpg",
+    price: 4.0,
+    rating: 4.65,
+    votes: 122,
+    popular: false,
+    available: false,
+  },
+  {
+    id: 6,
+    name: "Valentine Special",
+    image:
+      "https://csyxkpbavpcrhwqhcpyy.supabase.co/storage/v1/object/public/assets/coffee-challenge/valentine-special.jpg",
+    price: 5.5,
+    rating: null,
+    votes: 0,
+    popular: false,
+    available: true,
+  },
+];
