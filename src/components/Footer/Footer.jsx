254eab9b393784d52f05982fddd6b9962e081c9e
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-05 19:37:25 +0330

    update Footer.css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 export default function Footer() {
   return (
     <div className='footer-container'>
-        <h3 class="outhor">Made By <a href="">Ayoub Akartabar</a></h3>
+        <h3 class="author">Made By <a href="" className='author-link'>Ayoub Akartabar</a></h3>
     </div>
   )
 }
